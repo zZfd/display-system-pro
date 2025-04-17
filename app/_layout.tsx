addb95faca75e0ab57223c699d8c49290cf8022f
feida.zhang <feida.zhang@storehub.com>
2025-04-18 01:42:10 +0800

    feat: react-native-pager-view

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -54,10 +54,7 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="main" options={{ headerShown: false }} />
-
-        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: "modal" }} /> */}
+        <Stack.Screen name="index" options={{ headerShown: false }} />
       </Stack>
     </ThemeProvider>
   );
---
+++ app/index.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Image, StyleSheet, View } from "react-native";
+import PagerView from "react-native-pager-view";
+
+const ImageCarousel = () => {
+  const images = [
+    require("../assets/images/honey/honey-water-bg.png"),
+    require("../assets/images/honey/honey-box-bg.png"),
+  ];
+  const [currentPage, setCurrentPage] = useState(0);
+  const pageViewerRef = useRef<PagerView>(null);
+
+  // 自动轮播逻辑
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const nextPage = (currentPage + 1) % images.length;
+      // 使用 setPageWithoutAnimation 来确保动画方向一致
+      pageViewerRef.current?.setPageWithoutAnimation(nextPage);
+      setCurrentPage(nextPage);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [currentPage]);
+
+  const handlePageChange = (e: any) => {
+    const newPosition = e.nativeEvent.position;
+    // 处理用户手动滑动
+    if (newPosition < currentPage) {
+      // 向左滑动时，立即跳到最后一页
+      const lastPage = images.length - 1;
+      pageViewerRef.current?.setPageWithoutAnimation(lastPage);
+      setCurrentPage(lastPage);
+    } else {
+      setCurrentPage(newPosition);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <PagerView
+        ref={pageViewerRef}
+        style={styles.pageViewer}
+        initialPage={0}
+        onPageSelected={handlePageChange}
+        scrollEnabled={true}
+      >
+        {images.map((img, index) => (
+          <View key={index} style={styles.page}>
+            <Image source={img} style={styles.image} resizeMode="contain" />
+          </View>
+        ))}
+      </PagerView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  pageViewer: {
+    flex: 1,
+  },
+  page: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default ImageCarousel;
